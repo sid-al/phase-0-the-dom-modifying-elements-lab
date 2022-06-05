d9fd805ab2b54b7fdb16e40593d3f28b29152b9b
sid-al <sendplusx@gmail.com>
2022-06-05 18:16:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const element = document.querySelector('main');
+element.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'sid is champion'; 
