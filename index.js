b3c7d629215bdb3d201aa2a63679abd8a992b849
sid-al <sendplusx@gmail.com>
2022-06-13 08:36:23 +0300

    commit tree 92b941612e84f125a546ff174510b42670edfbc4

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ element.remove();
 
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.textContent = 'sid is champion'; 
+newHeader.textContent = 'sid is the champion'; 
